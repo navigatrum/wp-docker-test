6b58e754213055d58f0ca9b3c9729b39bfe5350a
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-10 16:01:51 +0200

    First commit

---
+++ wp-docker-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Docker Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-docker-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Docker_Test
+ */
+
+// Your code starts here.
