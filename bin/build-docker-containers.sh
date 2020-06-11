eeeb26adfc1131409866d88fe6b6bfb1e58f3fce
navigatrum <ab@Lenovo-PC.localdomain>
2020-06-11 04:56:09 +0200

    added start tp wp-tests docker service

--- bin/build-docker-containers.sh
+++ bin/build-docker-containers.sh
@@ -4,3 +4,4 @@ docker-compose run --rm mysql mysql --version
 docker-compose run --rm php php --version
 docker-compose run --rm php php -m
 docker-compose run --rm phpunit phpunit --version
+docker-compose up -d wp-tests
